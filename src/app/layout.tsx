618eb31ee65a172b87f9eb37714abb4eabd8955a
Miguel-UNSCH <miguelramirezgonzales48@gmail.com>
2024-10-31 18:21:53 -0500

    merge graficos 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { auth } from "@/auth";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SessionProvider } from "next-auth/react";
 
